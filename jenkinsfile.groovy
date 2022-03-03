d2e641a76779f2c1bfe0300fce8adf2a5cad3db7
Your Name <you@example.com>
2022-03-03 21:34:09 +0000

    another commit

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,8 +1,12 @@
 stage('Checkout external proj') {
         steps {
-            git branch: 'main',
-                url: 'git@github.com:snk-world/ci_task_11_5.git'
-
-            sh "ls -lat"
-        }
+        	 checkout([
+      			  $class: 'GitSCM', 
+		          branches: [[name: '*/main']], 
+		          doGenerateSubmoduleConfigurations: false, 
+  		          extensions: [[$class: 'CleanCheckout']], 
+ 		          submoduleCfg: [], 
+        		  userRemoteConfigs: [[credentialsId: '1', url: 'git@github.com:snk-world/ci_task_11_5.git']]
+		    ])
+	}
 }
