f1c7b562f4c5e9bcce62887e3ede4d5413337bbf
Your Name <you@example.com>
2022-03-03 21:42:21 +0000

    another commit

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -2,7 +2,7 @@
 node('worker_node1') {
 	stage('Source') {// Получение кода;
 		// Получаем код из нашего Git-репозитория;
-		git 'https://github.com/snk-world/ci_task_11_5.git’
+		git branch: 'main', url: 'https://github.com/snk-world/ci_task_11_5.git'
 	}
 	stage('Compile') {// Компиляция и выполнение модульного тестирования;
 		// Запуск Gradle для выполнения компиляции и модульного тестирования;
